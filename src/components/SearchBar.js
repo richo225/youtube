230571ea1aa49931e27dd7cbd2433f41c1f4dd7d
Richard Bates <richard.bates@babylonhealth.com>
2019-05-25 16:17:41 +0100

    Add SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
-const SearchBar = () => {
-  return <div> SearchBar</div>
+class SearchBar extends React.Component {
+  state = { value: '' };
+
+  handleChange = (event) => {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+  }
+
+  render() {
+    return(
+      <form className="ui form" onSubmit={this.handleSubmit}>
+        <input className="ui fluid icon input" type="text" placeholder="Search for a video" onChange={this.handleChange} />
+      </form>
+    );
+  }
 }
 
 export default SearchBar;
