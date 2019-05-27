4c5f02d1db002703e9f5f42d60e069015a65fd88
Richard Bates <richard.bates@babylonhealth.com>
2019-05-27 01:51:21 +0100

    Add video key, href and description

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -7,9 +7,12 @@ const VideoItem = (props) => {
         className="ui image"
         style={{ maxWidth: 240 }}
         src={props.video.snippet.thumbnails.medium.url}
+        alt={props.video.snippet.description}
       />
       <div className="content">
-        <a className="header">{ props.video.snippet.title }</a>
+        <a className="header" href={`https://www.youtube.com/watch?v=${props.video.id.videoId}`} >
+          { props.video.snippet.title }
+        </a>
         <div className="description">{props.video.snippet.channelTitle}</div>
       </div>
     </div>
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,7 +3,7 @@ import VideoItem from './VideoItem';
 
 const VideoList = (props) => {
   const renderedList = props.videos.map((video) => {
-    return <VideoItem video={video} />;
+    return <VideoItem video={video } key={video.id.videoId}  />
   });
 
   return <div className="ui relaxed divided list"> { renderedList } </div>
