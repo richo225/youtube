7bebe5cc0c76f20aa26625f296a31371421acd06
Richard Bates <richard.bates@babylonhealth.com>
2019-05-27 21:32:03 +0100

    Updates

---
+++ precache-manifest.b57f42d6690ebe6e78a3237ca0e19a62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c84a0ee64b8169f5b1acf44b01041999",
+    "url": "/youtube/index.html"
+  },
+  {
+    "revision": "5c661a0e228433596409",
+    "url": "/youtube/static/css/main.4a966e5b.chunk.css"
+  },
+  {
+    "revision": "7e6ac1444ef9242ebc00",
+    "url": "/youtube/static/js/2.824d5390.chunk.js"
+  },
+  {
+    "revision": "5c661a0e228433596409",
+    "url": "/youtube/static/js/main.eb8390f5.chunk.js"
+  },
+  {
+    "revision": "8e8db0effa94c990964e",
+    "url": "/youtube/static/js/runtime~main.a6ce4185.js"
+  }
+]);
